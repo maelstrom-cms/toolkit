fd3cd63cc07dd57c09fbd219841a45e916d16344
Owen Melbourne <owenmelbz@gmail.com>
2019-07-02 20:42:30 +0100

    📝 Documentation - Added some publishable stubs to help with quick start

--- src/Providers/MaelstromServiceProvider.php
+++ src/Providers/MaelstromServiceProvider.php
@@ -31,6 +31,13 @@ class MaelstromServiceProvider extends ServiceProvider
             __DIR__ . '/../config/maelstrom.php' => config_path('maelstrom.php'),
         ], 'maelstrom-config');
 
+        $this->publishes([
+            __DIR__ . '/../../ui/stubs/maelstrom.css.stub' => resource_path('sass/maelstrom.css'),
+            __DIR__ . '/../../ui/stubs/maelstrom.js.stub' => resource_path('js/maelstrom.js'),
+            __DIR__ . '/../../ui/stubs/babel.config.js.stub' => base_path('babel.config.js'),
+            __DIR__ . '/../../ui/stubs/webpack.mix.js.stub' => base_path('webpack.mix.js'),
+        ], 'maelstrom-stubs');
+
         $this->mergeConfigFrom(
             __DIR__ . '/../config/maelstrom.php',
             'maelstrom'
