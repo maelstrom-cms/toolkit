b0c3ee0a60a2f0da98a9375c7d251a95bee771af
Owen Melbourne <owenmelbz@gmail.com>
2019-09-12 10:25:00 +0100

    🔨 WIP - adjusted helper to return the parent if found - sorry purists

--- ui/js/support/HasParentClass.js
+++ ui/js/support/HasParentClass.js
@@ -1,4 +1,7 @@
 export default function hasParentClass(element, classname) {
-    if ((element.className || '').split(' ').indexOf(classname)>=0) return true;
+    if ((element.className || '').split(' ').indexOf(classname) >= 0) {
+        return element;
+    }
+
     return element.parentNode && hasParentClass(element.parentNode, classname);
 }
